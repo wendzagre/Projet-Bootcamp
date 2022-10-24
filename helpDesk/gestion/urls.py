b1eb39926d08d5a6a588004f0764855aa3155234
ZAGRE Patrick <patrickzagre3@gmail.com>
2022-10-24 23:12:42 +0000

    Mon Projet

---
+++ helpDesk/gestion/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path #Fonction Path
+from .import views #raccourci pour le repertoire courant
+
+#URL patterns pour chaque ligne
+urlpatterns=[
+    path('',views.ticket,name='ticket'),
+]
+
+# '': chaine vide et /
+# views.ticket : fonction ticket dans views.py
+# name ='ticket' : nom de la route
