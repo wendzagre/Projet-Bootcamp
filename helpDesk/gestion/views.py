7d807fbaddefaf4614f1bd158d6b2fd0043df57c
ZAGRE Patrick <patrickzagre3@gmail.com>
2022-10-25 21:01:48 +0000

    ticket

--- helpDesk/gestion/views.py
+++ helpDesk/gestion/views.py
@@ -3,6 +3,47 @@ from django.http import HttpResponse
 
 # Create your views here.
 def ticket(request):
-    return render(request,'ticket.html')
+    tickets=[
+        {
+            'Id':'1',
+            'Objet' :'Mise en place un réseau',
+            'Description':'SIGASPE',
+            'Type':'RESEAU',
+            'Statut':'Réglé',
+            'DateCréation':'12/4/2022',
+            'HeureCréation':'10h15'
+        },
+        {
+            'Id':'1',
+            'Objet' :'Mise en place un réseau',
+            'Description':'SIGASPE',
+            'Type':'RESEAU',
+            'Statut':'Réglé',
+            'DateCréation':'12/4/2022',
+            'HeureCréation':'10h15'
+        },
+        {
+            'Id':'1',
+            'Objet' :'Mise en place un réseau',
+            'Description':'SIGASPE',
+            'Type':'RESEAU',
+            'Statut':'Réglé',
+            'DateCréation':'12/4/2022',
+            'HeureCréation':'10h15'
+        },
+        {
+            'Id':'1',
+            'Objet' :'Mise en place un réseau',
+            'Description':'SIGASPE',
+            'Type':'RESEAU',
+            'Statut':'Réglé',
+            'DateCréation':'12/4/2022',
+            'HeureCréation':'10h15'
+        },
+    ]
+    context={
+          'tickets':tickets
+        }
+    return render(request,'ticket.html',context)
 
 
