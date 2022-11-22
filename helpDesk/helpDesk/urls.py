7077065e6fe249cfa2e95065c467908c00a1d54d
ZAGRE Patrick <patrickzagre3@gmail.com>
2022-11-22 22:43:42 +0000

    Connexion

--- helpDesk/helpDesk/urls.py
+++ helpDesk/helpDesk/urls.py
@@ -19,7 +19,7 @@ import authentication.views
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+   path('admin/', admin.site.urls),
     path('Agent/',include('Agent.urls')),
     path('gestion', include('gestion.urls')), #Inclure l'urls.py de l'application gestion
     path('',authentication.views.login_page,name='login') ,
